778a822d34ad7956d08c1c5d69e22c18f159fd0c
Amit Malhotra <amit08@gmail.com>
2015-02-25 01:01:17 -0500

    place move defined with sanity checks

--- board.py
+++ board.py
@@ -1,6 +1,8 @@
 __author__ = 'Amit'
 
 from exceptions import NotATileException
+from exceptions import NotAValidMoveException
+import pprint as pp
 
 
 class Board(object):
@@ -19,16 +21,44 @@ class Board(object):
             for m in range(y):
                 self.board[n].append(0)
 
+    def __str__(self):
+        """
+        Override of __str__()
+
+        :return: String representing the board
+        """
+        # ToDo: make a better represenation with headers for rows/columsn
+        return pp.pformat(self.board)
+
     def place_token(self, x, y, color):
         """
-        Places a token on the board at the given (x,y) coordinate with the given color
+        Places two tokens on the board at the given (x,y) coordinates for the given color.
+        The x represents the row of the board (A, B, C...)
+        The y represents the column of the board (1,2,3,4,5...).
+        if the tokens are white, they are placed vertically (Top, Down) and the coordinates
+        represent the top coordinate.
+        if the tokens are black, they are placed horizontally (Left, Right) and the coordinates
+        represent the left coordinate.
 
         :param x: integer representing the row of the board
         :param y: integer representing the column of the board
         :param color: a character representing the color of the token, either 'w' or 'b'
         :return: Nothing
         """
-        if color != 'w' or color != 'b':
+        if color is not 'w' and color is not 'b':
             raise NotATileException("This is not an expected tile color")
-        
-        self.board[x][y] = color
+
+        # check if (x,y), (x+1,y), (x,y+1) is occupied
+        if self.board[x][y] is not 0:
+            raise NotAValidMoveException("This tile is occupied, not a valid move")
+        if color is 'w' and self.board[x+1][y] is not 0:
+            raise NotAValidMoveException("This tile is occupied, not a valid move")
+        else:
+            self.board[x][y] = color
+            self.board[x+1][y] = color
+
+        if color is 'b' and self.board[x][y+1] is not 0:
+            raise NotAValidMoveException("This tile is occupied, not a valid move")
+        else:
+            self.board[x][y] = color
+            self.board[x][y+1] = color
